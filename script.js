5fa5419c6849ba1ea6d7baedd40f44ec9a858d45
Karan Kadakia <kkadakia@aggdirect.com>
2020-01-31 15:00:06 -0500

    added support for chrome browser on mobile

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/Face-Detection-JavaScript/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/Face-Detection-JavaScript/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/Face-Detection-JavaScript/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/Face-Detection-JavaScript/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('models')
 ]).then(startVideo)
 
 function startVideo() {
